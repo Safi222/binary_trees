0d1ed092506861a8c06fb7d9659d45d7ccbea19f
silgenius <olutademartin@gmail.com>
2024-06-04 07:07:17 -0700

    Fix betty requirement error

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -25,11 +25,11 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
  * @tree: A pointer to the root node of the tree to count the nodes
  * @size: A pointer to the size counter to update
  *
- * Description: This helper function traverses the tree in a 
- *              depth-first manner (pre-order traversal), and 
- *              increments the size counter each time a non-leaf 
+ * Description: This helper function traverses the tree in a
+ *              depth-first manner (pre-order traversal), and
+ *              increments the size counter each time a non-leaf
  *              node (a node with at least one child) is encountered.
- *              It can be called by a function designed to count 
+ *              It can be called by a function designed to count
  *              the non-leaf nodes in a binary tree.
  */
 void node_count(const binary_tree_t *tree, size_t *size)
