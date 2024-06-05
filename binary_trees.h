7e03d0b228c024de65d6593dfc4bbc99329a3b48
silgenius <olutademartin@gmail.com>
2024-06-05 09:31:53 -0700

    Update header file

--- binary_trees.h
+++ binary_trees.h
@@ -45,6 +45,7 @@ size_t binary_tree_depth(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 
 /* Helper function */
+size_t tree_height(const binary_tree_t *tree);
 void tree_size(const binary_tree_t *tree, size_t *size);
 void leaves_count(const binary_tree_t *tree, size_t *size);
 void node_count(const binary_tree_t *tree, size_t *size);
