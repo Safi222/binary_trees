21ea45a582ab07c4e8abcd99914e291aa3df01a3
silgenius <olutademartin@gmail.com>
2024-06-05 09:32:58 -0700

    Refactor code to pass unit test

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,18 +9,30 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height, right_height;
+	if (tree == NULL)
+		return (0);
+
+	return (tree_height(tree->left) -
+			tree_height(tree->right));
+}
+
+/**
+ * binary_tree_height - Computes the height of a binary tree.
+ *
+ * @tree: A pointer to the root node of the tree to measure the height.
+ *
+ * Return: The height of the binary tree as a size_t value.
+ *         If tree is NULL, returns 0.
+ */
+size_t tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
 
 	if (tree == NULL)
 		return (0);
 
-	left_height = 0;
-	right_height = 0;
+	left_height = tree_height(tree->left);
+	right_height = tree_height(tree->right);
 
-	if (tree->left)
-		left_height = binary_tree_balance(tree->left) + 1;
-	if (tree->right)
-		right_height = binary_tree_balance(tree->right) + 1;
-
-	return (left_height - right_height);
+	return (left_height > right_height ? left_height : right_height) + 1;
 }
