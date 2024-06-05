4de5c31bc96e22fcb5de79e75645116b1a53a1dd
silgenius <olutademartin@gmail.com>
2024-06-05 08:35:46 -0700

    Refactor code for efficiency

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth - Measures the depth of a node in a binary tree.
+ * tree_depth - Measures the depth of a node in a binary tree recursively.
  *
  * @tree: A pointer to the node to measure the depth.
  *
@@ -15,27 +15,10 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	depth = tree_depth(tree);
+	depth = 0;
 
-	return (depth - 1);
-}
+	if (tree->parent)
+		depth = binary_tree_depth(tree->parent) + 1;
 
-/**
- * tree_depth - Measures the depth of a node in a binary tree recursively.
- *
- * @tree: A pointer to the node to measure the depth.
- *
- * Return: The depth of the node as a size_t value.
- *         If tree is NULL, returns 0.
- */
-size_t tree_depth(const binary_tree_t *tree)
-{
-	size_t depth;
-
-	if (tree == NULL)
-		return (0);
-
-	depth = tree_depth(tree->parent);
-
-	return (depth + 1);
+	return (depth);
 }
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,36 +10,18 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height;
+	size_t left_height, right_height;
 
 	if (tree == NULL)
 		return (0);
 
-	height = tree_height(tree);
+	left_height = 0;
+	right_height = 0;
 
-	return (height - 1);
-}
+	if (tree->left)
+		left_height = binary_tree_height(tree->left) + 1;
+	if (tree->right)
+		right_height = binary_tree_height(tree->right) + 1;
 
-/**
- * tree_height - Computes the height of a binary tree.
- *
- * @tree: A pointer to the root node of the tree to measure the height.
- *
- * Return: The height of the binary tree as a size_t value.
- *         If tree is NULL, returns 0.
- */
-size_t tree_height(const binary_tree_t *tree)
-{
-	size_t left_depth, right_depth;
-
-	if (tree == NULL)
-		return (0);
-
-	left_depth = tree_height(tree->left);
-	right_depth = tree_height(tree->right);
-
-	if (left_depth > right_depth)
-		return (left_depth +  1);
-	else
-		return (right_depth + 1);
+	return (left_height > right_height ? left_height : right_height);
 }
