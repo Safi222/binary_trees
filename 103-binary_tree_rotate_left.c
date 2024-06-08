0b20356c1d43fd26106fe3a6b1993b5f24870c2c
martin olutade <112440359+silgenius@users.noreply.github.com>
2024-06-08 03:24:18 +0100

    Refactor code in 103-binary_tree_rotate_left.c to pass all unit test

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -19,7 +19,7 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
 	binary_tree_t *n_parent;
 
-	if (!tree || !tree->left)
+	if (!(tree || tree->right))
 		return (NULL);
 
 	/*Set the new parent for the tree that is right of the tree*/
