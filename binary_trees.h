85ddb8f8dd3166e5b0aa192d563b4b4e3b7b0206
silgenius <olutademartin@gmail.com>
2024-06-07 20:59:18 -0700

    Update header file

--- binary_trees.h
+++ binary_trees.h
@@ -49,6 +49,9 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+int binary_tree_is_complete(const binary_tree_t *tree);
+size_t node_size(const binary_tree_t *tree);
+int binary_complete(const binary_tree_t *tree, size_t index, size_t node_size);
 
 
 /* Helper function */
